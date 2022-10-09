1f4a1e19c6d2fd50ac675de00bdb0af0e23ca60c
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-10-10 00:10:22 +0300

    Add files via upload

---
+++ ToDoListCoreData/ToDoListCoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListCoreData
+//
+//  Created by Berkay Kuzu on 7.10.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoListCoreData/ToDoListCoreData/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoListCoreData
+//
+//  Created by Berkay Kuzu on 7.10.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
